d85f0b187b0c4a75b14b6b69be6fb57f86f524d7
SurekhaVadivel <surekhavadivel@gmail.com>
2023-01-18 11:41:02 -0600

    Sample

---
+++ JavaBasicCoding/src/com/n2s/basics/Sample.java
@@ -0,0 +1,11 @@
+package com.n2s.basics;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello Surekha");
+	}
+
+}
