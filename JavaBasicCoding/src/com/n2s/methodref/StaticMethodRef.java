0bfdc39edd95e7e04a4693f94aac351313ee82c3
SurekhaVadivel <surekhavadivel@gmail.com>
2023-02-06 11:07:34 -0600

    06-Feb-23

--- JavaBasicCoding/src/com/n2s/methodref/StaticMethodRef.java
+++ JavaBasicCoding/src/com/n2s/methodref/StaticMethodRef.java
@@ -7,13 +7,24 @@ public class StaticMethodRef {
 
 		Payment p = StaticMethodRef::creditPay;
 		p.pay();
+		
+		Dummy d = StaticMethodRef::testDummy;
+		d.test("Hello");
 	}
 
 	public static void creditPay(){
 		System.out.println("Payment using credit card");
 	}
+	
+	public static void testDummy(String s){
+		System.out.println(s);
+	}
 }
 
 interface Payment{
 	void pay();
+}
+
+interface Dummy{
+	void test(String s);
 }
